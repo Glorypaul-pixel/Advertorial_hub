a6923f2abf115e2f356d0c566290e97510f7d7d7
Glorypixel <ifeomaglory1998@gmail.com>
2025-05-07 20:34:17 +0100

    fixed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -8,10 +8,6 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
 });

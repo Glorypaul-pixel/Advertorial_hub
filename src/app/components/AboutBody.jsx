1abe98ac56b10298b2dcb991a03cc59e1eb61800
Glorypixel <ifeomaglory1998@gmail.com>
2025-09-27 01:37:45 +0100

    done

--- src/app/components/AboutBody.jsx
+++ src/app/components/AboutBody.jsx
@@ -1,4 +1,4 @@
-\ import React from "react";
+import React from "react";
 import "@/styles/AboutBody.css";
 const AboutBody = () => {
   return (
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,31 +6,26 @@ const Hero = () => {
   return (
     <div className="heroSection">
       {/* Hero Text Section */}
-      <div className="heroText">
-        <div className="heroHeadlineContainer" data-aos="fade-up">
-          {/* Left (2/3) */}
-          <div className="headline-text">
-            <h1>
-              Connect with Your Target Audience And <span>Drive Sales</span> With
-              AdvertorialHub
-            </h1>
+      <div className="heroBgSection">
+        {/* Overlay Text */}
+        <div className="heroText" data-aos="fade-up">
+          <h1>
+            Connect with Your Target Audience And <span>Drive Sales</span> With
+            AdvertorialHub
+          </h1>
 
-            <p data-aos="fade-right">
-              To reach the right people on social media, you need the right tools.{" "}
-              <br />
-              With <span>Advertorial Hub</span>, you{"'"}ve got everything you need!
-            </p>
-            <Link href="/authentication/CreateAccount">
-              <button className="HerobtnStart" data-aos="zoom-in">
-                Start Now
-              </button>
-            </Link>
-          </div>
+          <p data-aos="fade-right">
+            To reach the right people on social media, you need the right tools.{" "}
+            <br />
+            With <span>Advertorial Hub</span>, you{"'"}ve got everything you
+            need!
+          </p>
 
-          {/* Right (1/3) */}
-          <div className="headline-image">
-            <img src="/images/this.png" alt="headline visual" />
-          </div>
+          <Link href="/authentication/CreateAccount">
+            <button className="HerobtnStart" data-aos="zoom-in">
+              Start Now
+            </button>
+          </Link>
         </div>
       </div>
 
